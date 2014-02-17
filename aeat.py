692524154931d58c0b8e2a0934d4e084836ba29a
Albert Cervera i Areny <albert@nan-tic.com>
2014-02-17 09:29:46 +0100

    Remoe unused Date.

--- aeat.py
+++ aeat.py
@@ -194,7 +194,6 @@ class Report(Workflow, ModelSQL, ModelView):
         return self.company.currency.id
 
     def on_change_with_fiscalyear_code(self):
-        Date = Pool().get('ir.date')
         code = None
         if self.fiscalyear:
             code = self.fiscalyear.start_date.year
