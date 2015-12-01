f0c9f7be6c2f82b1e42c563e6e12d017e2721940
resteve <resteve@zikzakmedia.com>
2015-12-01 11:02:55 +0100

    Tests

--- tests/test_aeat_347.py
+++ tests/test_aeat_347.py
@@ -1,33 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the aeat_347 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class Aeat347TestCase(unittest.TestCase):
-    'Test AEAT 347 module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('aeat_347')
-
-    def test0005views(self):
-        'Test views'
-        test_view('aeat_347')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class Aeat347TestCase(ModuleTestCase):
+    'Test Aeat 347 module'
+    module = 'aeat_347'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            Aeat347TestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_aeat347.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+        Aeat347TestCase))
     return suite
