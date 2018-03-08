170ed00c68c4187abcfb048e1edff7fab64f3a50
Bernat Brunet Torruella <bernat@nan-tic.com>
2018-03-08 01:35:29 +0100

    Add include_347 field in tax template to update this field throw an xml (347.xml) on tax tempalte and after when update account char, update it on tax

--- tax.py
+++ tax.py
@@ -9,7 +9,25 @@ from trytond.transaction import Transaction
 from sql.operators import In
 from .aeat import OPERATION_KEY
 
-__all__ = ['Tax']
+__all__ = ['TaxTemplate', 'Tax']
+
+
+class TaxTemplate:
+    __metaclass__ = PoolMeta
+    __name__ = 'account.tax.template'
+
+    include_347 = fields.Boolean('Include 347')
+
+    @staticmethod
+    def default_include_347():
+        return True
+
+    def _get_tax_value(self, tax=None):
+        res = super(TaxTemplate, self)._get_tax_value(tax)
+
+        if not tax or tax.include_347 != self.include_347:
+            res['include_347'] = self.include_347
+        return res
 
 
 class Tax:
