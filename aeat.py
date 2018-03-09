3fae1879d8d2bef702c0c6f7a40fe952de10f82b
C?dric Krier <ced@b2ck.com>
2018-03-09 15:18:40 +0100

    Fix style

--- aeat.py
+++ aeat.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import itertools
 import datetime
 from decimal import Decimal
