9e6331c472ca2fae4c853b45df08c3a77713a703
C?dric Krier <ced@b2ck.com>
2018-03-09 15:18:40 +0100

    Fix style

--- tax.py
+++ tax.py
@@ -1,13 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond import backend
-from trytond.model import ModelSQL, ModelView, fields
-from trytond.wizard import Wizard, StateView, StateTransition, Button
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, Bool
-from trytond.transaction import Transaction
-from sql.operators import In
-from .aeat import OPERATION_KEY
+from trytond.model import fields
+from trytond.pool import PoolMeta
 
 __all__ = ['TaxTemplate', 'Tax']
 
