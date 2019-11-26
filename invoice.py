dc27cde0e2a296224216f0f76f6b23a6209b53b7
Bernat Brunet Torruella <bernat@nan-tic.com>
2019-11-26 10:15:20 +0100

    Change the 347 cehck from party to tax, and change the calculation of the 347 registers. Now it's possible to have some invoices wih lines added in the 347 and lines without.

--- invoice.py
+++ invoice.py
@@ -1,14 +1,13 @@
 # This file is part aeat_347 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond import backend
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.wizard import Wizard, StateView, StateTransition, Button
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, Bool
 from trytond.transaction import Transaction
 from sql.operators import In
 from .aeat import OPERATION_KEY
+##from trytond.modules.aeat_347.aeat import OPERATION_KEY
 
 __all__ = ['Record', 'Invoice', 'Recalculate347RecordStart',
     'Recalculate347RecordEnd', 'Recalculate347Record', 'Reasign347RecordStart',
@@ -80,64 +79,42 @@ class Record(ModelSQL, ModelView):
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
 
-    include_347 = fields.Boolean('Include 347')
-    aeat347_operation_key = fields.Selection([('', ''), ] + OPERATION_KEY,
-        'AEAT 347 Operation Key',
-        states={
-            'invisible': ~Bool(Eval('include_347')),
-            'required': Bool(Eval('include_347')),
-            },
-        depends=['include_347'])
+    aeat347_operation_key = fields.Selection(OPERATION_KEY,
+        'AEAT 347 Operation Key')
 
     @classmethod
     def __register__(cls, module_name):
-        pool = Pool()
-        Record = pool.get('aeat.347.record')
-        TableHandler = backend.get('TableHandler')
-
         cursor = Transaction().connection.cursor()
-        table = TableHandler(cls, module_name)
-        table_line = TableHandler(cls, 'account.invoice.line')
+        table = cls.__table_handler__(module_name)
         sql_table = cls.__table__()
-        record_table = Record.__table__()
 
         exist_347 = table.column_exist('include_347')
-
         super(Invoice, cls).__register__(module_name)
+        if exist_347:
+            table.drop_column('include_347')
+        cursor.execute(*sql_table.update(
+                columns=[sql_table.aeat347_operation_key],
+                values=['none'],
+                where=(sql_table.aeat347_operation_key == '')
+                | (sql_table.aeat347_operation_key == None)))
 
-        # Migration: moved 347 check mark from invoice line to invoice
-        if not exist_347:
-            cursor.execute(*record_table.select(record_table.invoice,
-                    record_table.operation_key))
-            for invoice_id, operation_key in cursor.fetchall():
-                cursor.execute(*sql_table.update(
-                        columns=[sql_table.include_347,
-                            sql_table.aeat347_operation_key],
-                        values=[True, operation_key],
-                        where=sql_table.id == invoice_id))
-            table_line.drop_column('include_347')
-            table_line.drop_column('aeat347_operation_key')
+    @classmethod
+    def __setup__(cls):
+        super(Invoice, cls).__setup__()
+        cls._check_modify_exclude += ['aeat347_operation_key']
 
-    def on_change_party(self):
-        super(Invoice, self).on_change_party()
-        self.include_347 = self.on_change_with_include_347()
-        self.aeat347_operation_key = \
-            self.on_change_with_aeat347_operation_key()
+    @staticmethod
+    def default_aeat347_operation_key():
+        return None
 
-    @fields.depends('party')
-    def on_change_with_include_347(self, name=None):
-        return self.party.include_347 if self.party else False
-
-    @fields.depends('type', 'aeat347_operation_key', 'include_347')
+    @fields.depends('type', 'aeat347_operation_key')
     def on_change_with_aeat347_operation_key(self):
-        if not self.include_347:
-            return ''
         if self.aeat347_operation_key:
             return self.aeat347_operation_key
         if self.type:
             return self.get_aeat347_operation_key(self.type)
         else:
-            return ''
+            return None
 
     @classmethod
     def get_aeat347_operation_key(cls, invoice_type):
@@ -149,10 +126,11 @@ class Invoice(metaclass=PoolMeta):
 
         amount = 0
         for tax in self.taxes:
-            if tax.tax.include_347 and (hasattr(tax.tax, 'recargo_equivalencia')
-                    and tax.tax.recargo_equivalencia):
+            if tax.tax.operation_347 in ('ignore', 'exclude_invoice'):
+                continue
+            if tax.tax.operation_347 == 'amount_only':
                 amount += tax.amount
-            elif tax.tax.include_347:
+            elif tax.tax.operation_347 == 'base_amount':
                 amount += (tax.base + tax.amount)
         if amount > self.total_amount:
             amount = self.total_amount
@@ -163,10 +141,22 @@ class Invoice(metaclass=PoolMeta):
         return amount
 
     def check_347_taxes(self):
+        include = False
         for tax in self.taxes:
-            if not tax.tax.include_347:
+            if tax.tax.operation_347 == 'exclude_invoice':
                 return False
-        return True
+            if tax.tax.operation_347 != 'ignore':
+                include = True
+        return include
+
+    @fields.depends('type', 'aeat347_operation_key')
+    def _on_change_lines_taxes(self):
+        super(Invoice, self)._on_change_lines_taxes()
+        if not self.check_347_taxes():
+            self.aeat347_operation_key = None
+        elif not self.aeat347_operation_key:
+            self.aeat347_operation_key = self.get_aeat347_operation_key(
+                self.type)
 
     @classmethod
     def create_aeat347_records(cls, invoices):
@@ -177,12 +167,17 @@ class Invoice(metaclass=PoolMeta):
         to_create = {}
         to_update = []
         for invoice in invoices:
-            if (not invoice.move or invoice.state == 'cancel' or
-                    not invoice.include_347):
+            if (not invoice.move or invoice.state == 'cancel'):
                 continue
             if not invoice.check_347_taxes():
+                invoice.aeat347_operation_key = None
                 to_update.append(invoice)
                 continue
+            if not invoice.aeat347_operation_key:
+                invoice.aeat347_operation_key = \
+                    invoice.get_aeat347_operation_key(invoice.type)
+                to_update.append(invoice)
+
             if invoice.aeat347_operation_key:
                 operation_key = invoice.aeat347_operation_key
                 amount = invoice.get_aeat347_total_amount()
@@ -209,10 +204,8 @@ class Invoice(metaclass=PoolMeta):
 
         Record.delete_record(invoices)
         with Transaction().set_context(check_modify_invoice=False):
-            cls.write(to_update, {
-                    'aeat347_operation_key': None,
-                    'include_347': False,
-                    })
+            cls.save(to_update)
+            #cls.save(cls.browse([x.id for x in to_update]))
         with Transaction().set_user(0, set_context=True):
             Record.create(to_create.values())
 
@@ -221,34 +214,6 @@ class Invoice(metaclass=PoolMeta):
         check =Transaction().context.get('check_modify_invoice', True)
         if check:
             super(Invoice, cls).check_modify(invoices)
-
-    @classmethod
-    def create(cls, vlist):
-        Party = Pool().get('party.party')
-
-        vlist = [x.copy() for x in vlist]
-
-        party_ids = set()
-        for vals in vlist:
-            if 'include_347' not in vals:
-                party_ids.add(vals['party'])
-
-        if party_ids:
-            with Transaction().set_context(active_test=False):
-                parties = dict([(x.id, x.include_347) for x in Party.search([
-                            ('id', 'in', list(party_ids))])])
-
-        for vals in vlist:
-            if 'include_347' not in vals:
-                party_id = vals['party']
-                vals['include_347'] = parties[party_id]
-            if not vals.get('include_347', True):
-                continue
-            invoice_type = vals.get('type', 'out')
-            vals['aeat347_operation_key'] = cls.get_aeat347_operation_key(
-                invoice_type)
-
-        return super(Invoice, cls).create(vlist)
 
     @classmethod
     def draft(cls, invoices):
@@ -313,13 +278,13 @@ class Reasign347RecordStart(ModelView):
     """
     __name__ = "aeat.347.reasign.records.start"
 
-    aeat_347_operation_key = fields.Selection([('none', 'Leave Empty'), ] +
-        OPERATION_KEY, 'Operation Key', required=True)
+    aeat347_operation_key = fields.Selection(OPERATION_KEY, 'Operation Key',
+        required=True)
     include_347 = fields.Boolean('Include 347')
 
     @staticmethod
-    def default_aeat_347_operation_key():
-        return 'none'
+    def default_aeat347_operation_key():
+        return None
 
 
 class Reasign347RecordEnd(ModelView):
@@ -351,16 +316,11 @@ class Reasign347Record(Wizard):
         invoice_ids = Transaction().context['active_ids']
         invoices = Invoice.browse(invoice_ids)
 
-        value = self.start.aeat_347_operation_key
-        include = self.start.include_347
-        if value == 'none' or not include:
-            value = None
-
+        value = self.start.aeat347_operation_key
         invoice = Invoice.__table__()
         # Update to allow to modify key for posted invoices
-        cursor.execute(*invoice.update(columns=[invoice.aeat347_operation_key,
-                    invoice.include_347],
-                values=[value, include], where=In(invoice.id, invoice_ids)))
+        cursor.execute(*invoice.update(columns=[invoice.aeat347_operation_key,],
+                values=[value], where=In(invoice.id, invoice_ids)))
 
         Invoice.create_aeat347_records(invoices)
 
--- tax.py
+++ tax.py
@@ -3,58 +3,34 @@
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
-__all__ = ['TaxTemplate', 'Tax', 'TaxRuleTemplate', 'TaxRule']
+__all__ = ['TaxTemplate', 'Tax']
+
+OPERATION_347 = [
+    (None, ''),
+    ('base_amount', 'Include Base Amount'),
+    ('amount_only', 'Amount Only'),
+    ('ignore', 'Ignore'),
+    ('exclude_invoice', 'Exclude Invoice')]
 
 
 class TaxTemplate(metaclass=PoolMeta):
     __name__ = 'account.tax.template'
 
-    include_347 = fields.Boolean('Include 347')
+    operation_347 = fields.Selection(OPERATION_347, 'Operation for 347')
 
     @staticmethod
-    def default_include_347():
-        return False
+    def default_operation_347():
+        return 'ignore'
 
     def _get_tax_value(self, tax=None):
         res = super(TaxTemplate, self)._get_tax_value(tax)
 
-        if not tax or tax.include_347 != self.include_347:
-            res['include_347'] = self.include_347
+        if not tax or tax.operation_347 != self.operation_347:
+            res['operation_347'] = self.operation_347
         return res
 
 
 class Tax(metaclass=PoolMeta):
     __name__ = 'account.tax'
 
-    include_347 = fields.Boolean('Include 347')
-
-    @staticmethod
-    def default_include_347():
-        return False
-
-
-class TaxRuleTemplate(metaclass=PoolMeta):
-    __name__ = 'account.tax.rule.template'
-
-    include_347 = fields.Boolean('Include 347')
-
-    @staticmethod
-    def default_include_347():
-        return False
-
-    def _get_tax_rule_value(self, rule=None):
-        res = super(TaxRuleTemplate, self)._get_tax_rule_value(rule)
-
-        if not rule or rule.include_347 != self.include_347:
-            res['include_347'] = self.include_347
-        return res
-
-
-class TaxRule(metaclass=PoolMeta):
-    __name__ = 'account.tax.rule'
-
-    include_347 = fields.Boolean('Include 347')
-
-    @staticmethod
-    def default_include_347():
-        return False
+    operation_347 = fields.Selection(OPERATION_347, 'Operation for 347')
